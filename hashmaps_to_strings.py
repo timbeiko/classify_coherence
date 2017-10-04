dd67c26bc8bde8883c69eb58d69e3c3e5363a836
tbeiko <t.beiko23@gmail.com>
2017-10-04 11:51:46 -0400

    rename preprocess, and create a dictionary of tokens appearing in sentences

--- hashmaps_to_strings.py
+++ hashmaps_to_strings.py
@@ -1,6 +1,11 @@
-# This takes the output of preprocess.py and converts it to a format acceptable for Syntaxnet. 
+# This takes the output of preprocess.py and converts it to raw text
+# While also keeping track of sentence lengths and creating a dictionary of words 
 import os 
+import nltk 
 import json 
+
+dictionary = {}
+max_sentence_length = 0 
 
 for filename in os.listdir(os.getcwd()+ "/data"):
     # Skip input data to preprocess.py and generated .txt files 
@@ -20,3 +25,15 @@ for filename in os.listdir(os.getcwd()+ "/data"):
     for line in data:
         sentence = line['Arg1Raw'] + " " + line['ConnectiveRaw'] + " " + line['Arg2Raw'] + "\n"
         out.write(sentence)
+
+        word_sentence = nltk.word_tokenize(sentence.lower())
+        if len(word_sentence) > max_sentence_length:
+            max_sentence_length = len(word_sentence)
+        for word in word_sentence:
+            if word not in dictionary:
+                dictionary[word] = 1
+            else:
+                dictionary[word] += 1 
+
+print len(dictionary.keys())
+print max_sentence_length
