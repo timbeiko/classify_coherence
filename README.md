e5b00cf3727f8fba53c8bcb17e3576b0f45753b1
tbeiko <t.beiko23@gmail.com>
2017-10-13 11:25:22 -0400

    Get CNN to work --- accuracy is horrible

--- README.md
+++ README.md
@@ -22,3 +22,29 @@ A detailled (in progress) report of this project can be found at https://www.ove
 
 - To get a single parsed sentence from 'parses.json', read it into 'data' and then: `data[0]['wsj_2276']['sentences'][0]`
 - CNN Model built from https://github.com/dennybritz/cnn-text-classification-tf
+- To analyze TF vocabulary:
+```
+import numpy as np
+from tensorflow.contrib import learn
+
+x_text = ['This is a cat','This must be boy', 'This is a a dog']
+max_document_length = max([len(x.split(" ")) for x in x_text])
+
+## Create the vocabularyprocessor object, setting the max lengh of the documents.
+vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
+
+## Transform the documents using the vocabulary.
+x = np.array(list(vocab_processor.fit_transform(x_text)))    
+
+## Extract word:id mapping from the object.
+vocab_dict = vocab_processor.vocabulary_._mapping
+
+## Sort the vocabulary dictionary on the basis of values(id).
+## Both statements perform same task.
+#sorted_vocab = sorted(vocab_dict.items(), key=operator.itemgetter(1))
+sorted_vocab = sorted(vocab_dict.items(), key = lambda x : x[1])
+
+## Treat the id's as index into list and create a list of words in the ascending order of id's
+## word with id i goes at index i of the list.
+vocabulary = list(list(zip(*sorted_vocab))[0])
+``` 
--- train.py
+++ train.py
@@ -1,97 +1,208 @@
+# Flags and training loop from https://github.com/dennybritz/cnn-text-classification-tf/blob/master/train.py
 import tensorflow as tf
 import numpy as np
 import os
 import time
 import datetime
-import data_helpers
-from text_cnn import TextCNN
+from cnn import TextCNN
 from tensorflow.contrib import learn
+
+# Flags
+# ==================================================
+# Data loading params
+tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
+tf.flags.DEFINE_string("coherent_data_file", "./data/padded/coherent_sentences.txt", "Data source for the coherent data.")
+tf.flags.DEFINE_string("incoherent_data_file", "./data/padded/incoherent_sentences_arg2_random.txt", "Data source for the incoherent data.")
+
+# Model Hyperparameters
+tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
+tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
+tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
+tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
+tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
+
+# Training parameters
+tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
+tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
+tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
+tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
+tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
+# Misc Parameters
+tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
+tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+
+FLAGS = tf.flags.FLAGS
+FLAGS._parse_flags()
+print("\nParameters:")
+for attr, value in sorted(FLAGS.__flags.items()):
+    print("{}={}".format(attr.upper(), value))
+print("")
+
+
+# Data Preparation
+# ==================================================
+
+# Load data
+coherent_sentences = list(open(FLAGS.coherent_data_file, 'r').readlines())
+coherent_sentences = [c.strip() for c in coherent_sentences]
+incoherent_sentences = list(open(FLAGS.incoherent_data_file, 'r').readlines())
+incoherent_sentences = [c.strip() for c in incoherent_sentences]
+x_text = coherent_sentences + incoherent_sentences
+
+# Build vocabulary 
+### --- NOT SURE ABOUT THIS, GETTING MANY UNK TOKENS
+### --- PERHAPS BETTER TO USE MY OWN CONVERSION WITH THE INT SENTENCES
+### Seems to be an issue caused by tensorflow not including symbols such as '$' and '%' in its vocab
+### see README.md for notes on analyzing vocabulary
+max_document_length = max([len(x.split(" ")) for x in x_text])
+vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
+x = np.array(list(vocab_processor.fit_transform(x_text)))
+
+# Labels: [1, 0]: coherent, [0, 1]: incoherent
+coherent_labels = [[0, 1] for _ in coherent_sentences]
+incoherent_labels = [[1, 0] for _ in incoherent_sentences]
+y = np.concatenate([coherent_labels, incoherent_labels])
+
+# Shuffle Data
+np.random.seed(10)
+shuffle_indices = np.random.permutation(np.arange(len(y)))
+x_shuffled = x[shuffle_indices]
+y_shuffled = y[shuffle_indices]
+
+# Split train/test set
+dev_sample_index = -1 * int(FLAGS.dev_sample_percentage * float(len(y)))
+x_train, x_dev = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
+y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
+print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
+print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
+
+# Training
+# ==================================================
 
 with tf.Graph().as_default():
     session_conf = tf.ConfigProto(
-        allow_soft_placement=FLAGS.allow_soft_placement,
-        log_device_placement=FLAGS.log_device_placement)
+      allow_soft_placement=FLAGS.allow_soft_placement,
+      log_device_placement=FLAGS.log_device_placement)
     sess = tf.Session(config=session_conf)
-
     with sess.as_default():
         cnn = TextCNN(
-            sequence_length = x_train.shape[1],
-            num_classes=2,
-            vocab_size=len(vocabulary),
+            sequence_length=x_train.shape[1],
+            num_classes=y_train.shape[1],
+            vocab_size=len(vocab_processor.vocabulary_),
             embedding_size=FLAGS.embedding_dim,
-            filter_sizes=map(int, FLAGS.filter_sizes.split(",")),
-            num_filters=FLAGS.num_filters)
+            filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),
+            num_filters=FLAGS.num_filters,
+            l2_reg_lambda=FLAGS.l2_reg_lambda)
 
+        # Define Training procedure
         global_step = tf.Variable(0, name="global_step", trainable=False)
-        optimizer = tf.train.AdamOptimizer(1e-4)
+        optimizer = tf.train.AdamOptimizer(1e-3)
         grads_and_vars = optimizer.compute_gradients(cnn.loss)
-        train_op = optimizer.apply_gradient(grads_and_vars, global_step=global_step)
+        train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
 
-        # Summaries 
+        # Keep track of gradient values and sparsity (optional)
+        grad_summaries = []
+        for g, v in grads_and_vars:
+            if g is not None:
+                grad_hist_summary = tf.summary.histogram("{}/grad/hist".format(v.name), g)
+                sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(v.name), tf.nn.zero_fraction(g))
+                grad_summaries.append(grad_hist_summary)
+                grad_summaries.append(sparsity_summary)
+        grad_summaries_merged = tf.summary.merge(grad_summaries)
+
+        # Output directory for models and summaries
         timestamp = str(int(time.time()))
         out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
-        print("Writting to {}\n".format(out_dir))
+        print("Writing to {}\n".format(out_dir))
 
-        loss_summary = tf.scalar_summary("loss", cnn.loss)
-        acc_summary = tf.scalar_summary("accuracy", cnn.accuracy)
+        # Summaries for loss and accuracy
+        loss_summary = tf.summary.scalar("loss", cnn.loss)
+        acc_summary = tf.summary.scalar("accuracy", cnn.accuracy)
 
-        # Train summaries
-        train_summary_op = tf.merge_summary([loss_summary, acc_summary])
+        # Train Summaries
+        train_summary_op = tf.summary.merge([loss_summary, acc_summary, grad_summaries_merged])
         train_summary_dir = os.path.join(out_dir, "summaries", "train")
-        train_summary_writer = tf.train.SummaryWriter(train_summary_dir, sess.graph_def)
+        train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
 
-        # Dev summaries 
-        dev_summary_op = tf.merge(summary([loss_summary, acc_summary]))
+        # Dev summaries
+        dev_summary_op = tf.summary.merge([loss_summary, acc_summary])
         dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
-        dev_summary_writter = tf.train.SummaryWriter(dev_summary_dir, sess.graph_def)
+        dev_summary_writer = tf.summary.FileWriter(dev_summary_dir, sess.graph)
 
-        # Checkpointing
+        # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
         checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
         checkpoint_prefix = os.path.join(checkpoint_dir, "model")
         if not os.path.exists(checkpoint_dir):
             os.makedirs(checkpoint_dir)
-        saver = tf.train.Saver(tf.all_variables())
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=FLAGS.num_checkpoints)
 
-        # Initialize all variables 
+        # Write vocabulary
+        vocab_processor.save(os.path.join(out_dir, "vocab"))
+
+        # Initialize all variables
         sess.run(tf.global_variables_initializer())
 
         def train_step(x_batch, y_batch):
+            """
+            A single training step
+            """
             feed_dict = {
-                cnn.input_x: x_batch,
-                cnn.input_y: y_batch,
-                cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
+              cnn.input_x: x_batch,
+              cnn.input_y: y_batch,
+              cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
             }
             _, step, summaries, loss, accuracy = sess.run(
                 [train_op, global_step, train_summary_op, cnn.loss, cnn.accuracy],
                 feed_dict)
             time_str = datetime.datetime.now().isoformat()
-            print ("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
             train_summary_writer.add_summary(summaries, step)
 
-
         def dev_step(x_batch, y_batch, writer=None):
+            """
+            Evaluates model on a dev set
+            """
             feed_dict = {
-                cnn.input_x: x_batch,
-                cnn.input_y: y_batch,
-                cnn.dropout_keep_prob: 1.0
+              cnn.input_x: x_batch,
+              cnn.input_y: y_batch,
+              cnn.dropout_keep_prob: 1.0
             }
-
-            step, summaries_loss, accuracy = sess.run(
+            step, summaries, loss, accuracy = sess.run(
                 [global_step, dev_summary_op, cnn.loss, cnn.accuracy],
                 feed_dict)
             time_str = datetime.datetime.now().isoformat()
-            print ("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))       
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
             if writer:
                 writer.add_summary(summaries, step)
 
-        # Training loop 
-        batches = data_helpers.batch_iter(
-            zip(x_train, y_train), FLAGS.batch_size, FLAGS.num_epochs)
+        def batch_iter(data, batch_size, num_epochs, shuffle=True):
+            """
+            Generates a batch iterator for a dataset.
+            """
+            data = np.array(data)
+            data_size = len(data)
+            num_batches_per_epoch = int((len(data)-1)/batch_size) + 1
+            for epoch in range(num_epochs):
+                # Shuffle the data at each epoch
+                if shuffle:
+                    shuffle_indices = np.random.permutation(np.arange(data_size))
+                    shuffled_data = data[shuffle_indices]
+                else:
+                    shuffled_data = data
+                for batch_num in range(num_batches_per_epoch):
+                    start_index = batch_num * batch_size
+                    end_index = min((batch_num + 1) * batch_size, data_size)
+                    yield shuffled_data[start_index:end_index]
 
+        # Generate batches
+        batches = batch_iter(
+            list(zip(x_train, y_train)), FLAGS.batch_size, FLAGS.num_epochs)
+        
+        # Training loop. For each batch...
         for batch in batches:
             x_batch, y_batch = zip(*batch)
             train_step(x_batch, y_batch)
-            current_step - tf.train.global_step(sess, global_step)
+            current_step = tf.train.global_step(sess, global_step)
             if current_step % FLAGS.evaluate_every == 0:
                 print("\nEvaluation:")
                 dev_step(x_dev, y_dev, writer=dev_summary_writer)
